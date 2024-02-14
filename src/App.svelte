a9cd339978793c9b50a484812bbe63793766e799
Julien Grob <grob.julien1@gmail.com>
2024-02-14 18:56:16 +0100

    wip

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+	let people = [
+		{name: 'Gael', beltcolour: 'black', age: 24, id: 1},
+		{name: 'William', beltcolour: 'black', age: 24, id: 2},
+		{name: 'Martin', beltcolour: 'black', age: 23, id: 3}
+	];
+//   let firstName = 'Julien';
+//   let lastName = 'Grob';
+//   let beltColour = 'black'
+
+//   $: fullName = `${firstName} ${lastName}`;
+//   $: {
+// 	console.log(beltColour);
+// 	console.log(fullName);
+//   };
+//   const handleClick =() => {
+// 	if (beltColour =='orange'){
+// 		beltColour ='black';
+// 	}
+// 	else{
+// 	beltColour='orange';
+// 	}
+//   };
+//   const handleInput=(e)=> {
+// 	beltColour = e.target.value ;
+//   };
+</script>
+
+<main>
+	{#each people as person (person.id)}
+		
+	<div>
+		<h4>{person.name}</h4>
+		<p>{person.age} years old, {person.beltColour} belt.</p>
+	  </div>
+	  {/each}
+	<!-- <h1>Hello {name}!</h1> -->
+	<!-- <p style ="color: {beltColour}">{firstName} {lastName} - {beltColour} belt</p>
+	<input type="text" bind:value={beltColour}>
+	<input type="text" bind:value={firstName}>
+	<input type="text" bind:value={lastName}> -->
+	<!-- <button on:click={handleClick}>update belt colour</button> -->
+	<!-- <input type="text" on:input={handleInput} value={beltColour}>-->
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
